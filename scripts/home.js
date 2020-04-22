b7611ebdd04a49ba33b24a8e6228d2cb595a0fcb
arnaudmg <arno.mege@gmail.com>
2020-04-23 01:13:39 +0200

    Adjusting

--- scripts/home.js
+++ scripts/home.js
@@ -8,6 +8,5 @@ var parallaxInstance = new Parallax(scene);
 
 document.querySelector(".js-toggler").addEventListener("change", (_event) => {
     document.body.classList.toggle("dark-mode")
-    document.querySelector('.home').classList.toggle("active")
-
+    document.querySelector('.home').classList.toggle("active") 
 })
