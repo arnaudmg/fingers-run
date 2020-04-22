a424d211f1eb40e1a1a6550bda87e62d1b716542
arnaudmg <arno.mege@gmail.com>
2020-04-22 11:28:50 +0200

    Your new message here

--- scripts/home.js
+++ scripts/home.js
@@ -2,3 +2,6 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
