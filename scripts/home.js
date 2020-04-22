25d4d7db044b38febaddd29d3ebff6d86019a717
arnaudmg <arno.mege@gmail.com>
2020-04-23 00:38:51 +0200

    Dark mode Toggle

--- scripts/home.js
+++ scripts/home.js
@@ -5,3 +5,9 @@ function myFunction() {
 
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+document.querySelector(".js-toggler").addEventListener("change", (_event) => {
+    document.body.classList.toggle("dark-mode")
+    document.querySelector('.home').classList.toggle("active")
+
+})
