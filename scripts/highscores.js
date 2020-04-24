6c3878366a0145e8e04a22e4374e48369c837a2f
arnaudmg <arno.mege@gmail.com>
2020-04-24 02:37:22 +0200

    improvements

---
+++ scripts/highscores.js
@@ -0,0 +1,11 @@
+const leaderBoard = JSON.parse(localStorage.getItem("highscore"))
+const leaderboardElement = document.querySelector("#highScoresList")
+const entryPlaceholder = document.querySelector(".high-score.js-placeholder")
+const entryModel = entryPlaceholder.cloneNode()
+entryModel.classList.remove("js-placeholder")
+entryPlaceholder.remove()
+for(let [username, score] of Object.entries(leaderBoard)){
+    const entry = entryModel.cloneNode()
+    entry.innerHTML = `${username} - ${score}`
+    leaderboardElement.appendChild(entry)
+}
