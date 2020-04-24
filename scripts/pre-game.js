aa2571f089f3ba7c36d2c917d09476f995854249
arnaudmg <arno.mege@gmail.com>
2020-04-24 02:39:17 +0200

    Files adjustments

--- scripts/pre-game.js
+++ scripts/pre-game.js
@@ -2,3 +2,38 @@ document.querySelector(".js-toggler").addEventListener("change", (_event) => {
     document.body.classList.toggle("dark-mode")
     document.querySelector('.pictureBackground').classList.toggle("active") 
 })
+
+const usernameInput = document.querySelector("input#username")
+
+usernameInput.value = localStorage.getItem("username") ? localStorage.getItem("username") : ""
+
+let cocoselected = document.querySelector('.coco a')
+cocoselected.addEventListener('click', (event) => {
+    if (usernameInput.value.length === 0) {
+        event.preventDefault()
+        alert("Veuillez entrer un nom avant de continuer")
+        return
+    }
+    localStorage.setItem("username", usernameInput.value)
+    localStorage.setItem("character", "coco")
+})
+let sgselected = document.querySelector('.superGirl')
+sgselected.addEventListener('click', (event) => {
+    if (usernameInput.value.length === 0) {
+        event.preventDefault()
+        alert("Veuillez entrer un nom avant de continuer")
+        return
+    }
+    localStorage.setItem("username", usernameInput.value)
+    localStorage.setItem("character", "supergirl")
+})
+let wmselected = document.querySelector('.watermelon')
+wmselected.addEventListener('click', (event) => {
+    if (usernameInput.value.length === 0) {
+        event.preventDefault()
+        alert("Veuillez entrer un nom avant de continuer")
+        return
+    }
+    localStorage.setItem("username", usernameInput.value)
+    localStorage.setItem("character", "watermelon")
+})
